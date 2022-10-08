40e4949c94727e9fde801c1370e43be9ffc53a0e
Kicey <1569636100@qq.com>
2022-10-08 19:40:01 +0800

    gateway: zuul to webflux (static file routing problem exist)

--- bookstore-microservices-platform-gateway/src/main/java/com/github/fenixsoft/bookstore/gateway/GatewayApplication.java
+++ bookstore-microservices-platform-gateway/src/main/java/com/github/fenixsoft/bookstore/gateway/GatewayApplication.java
@@ -3,15 +3,13 @@ package com.github.fenixsoft.bookstore.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * SpringCloud Netflix全家桶的服务API网关
  *
  * @author icyfenix@gmail.com
- * @date 2020/4/17 17:50
+ * @date 2020/4/4 16:12
  **/
-@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
